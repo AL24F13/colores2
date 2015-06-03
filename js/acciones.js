4c14d1822760393b29c1616537bc543222d7ccd2
AL24F13 <mortallrulax27@gmail.com>
2015-06-03 14:38:44 -0500

    Aplicacion07
    juego

--- js/acciones.js
+++ js/acciones.js
@@ -2,38 +2,50 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX ('B1','audio/C.mp3',function(){}, function (msg) { alert ("Error " +         msg );});
-	audio.preloadFX ('B2','audio/D.mp3',function(){}, function (msg) { alert ("Error " +         msg );});
-	audio.preloadFX ('B3','audio/E.mp3',function(){}, function (msg) { alert ("Error " +         msg );});
-	audio.preloadFX ('B4','audio/F.mp3',function(){}, function (msg) { alert ("Error " +         msg );});
-	$('#btn_jugar').on('click', function(){
-		var pantalla = $.mobile.getScreenHeight();
-		var encabezado = $('.ui-header').outerHeight();
-		var pie = $('.ui-footer').outerHeight();
-		var contenido = $('ui.content').outerHeight();
-		var alto = (pantalla - encabezado - pie)/2;
-		$('.cuadro').height(alto);
-		});//btnjugar.click
-		
-		$('.cuadro').on('mousedown', function(){
-			$(this).addClass('pulsado');
-			});//mousedown
-		
-		function quien (q)
-	{
-		audio.play(q);
-		return q.substring(1);
-	}	
-		
-		$('.cuadro').on('mouseup', function (){
-			$('#pantalla').append(quien($(this).attr('id')));
-			$(this).removeClass('pulsado');
-		});
-		
-		
+	audio.preloadFX ('B1','audio/C.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX ('B2','audio/D.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX ('B3','audio/E.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX ('B4','audio/F.mp3',function() {},
+	function(msg){alert("error "+msg);});
 	
-	//div	
-	//div
-}); 
+$('#btnjugar').on ('tap',function(){
+
+	var pantalla=$.mobile.getScreenHeight();
+	var encabezado=$('.ui-header').outerHeight();
+	var pie=$('.ui-footer').outerHeight();
+	var contenido=$('.ui-content').outerHeight();
+	//alert ('pantalla ' + pantalla);
+	//alert ('encabezado ' + encabezado);
+	//alert ('pie' + pie);
+	//alert ('contenido ' + contenido);
+	var alto=(pantalla-encabezado-pie)/2;
+	$('.cuadro').height(alto);
+	
+	
+	
+});//btnjugar
+
+
+$('.cuadro').on ('vmousedown', function(){
+	
+	$('#pantalla').append (quien ($ (this).attr('id')));
+	$(this).addClass('pulsado');
+	});	
+	
+	$('.cuadro').on ('vmouseup', function(){
+	$(this).removeClass('pulsado');	
+    });	
+	
+});//cuadro
+
+function quien (q)
+{
+ audio.play(q);
+ return q.substring(1);
+}
+
 });
- 
+
